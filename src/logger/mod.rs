217f35b019c79e5eeab0e96422a755e1751e4b58
Lethon <i@frostmiku.com>
2021-02-22 17:26:02 +0800

    update log style

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -1,15 +1,32 @@
+use std::io::Write;
+use chrono::Local;
 use log::LevelFilter;
-use env_logger::{Builder, Target};
+use env_logger::{Builder, Target, fmt::Color};
 
 pub fn init() {
-    // let format = |buf:&mut Formatter,record: &Record| {
-    //     writeln!(buf, "{}: {}", record.level(), record.args())
-    // };
  
     let mut builder = Builder::new();
     builder.target(Target::Stdout).filter(None, LevelFilter::Debug);
 
-    // builder.format(format);
+    builder.format(|buf, record| {
+
+        let color = match record.level() {
+            log::Level::Info => Color::Green,
+            log::Level::Warn => Color::Yellow,
+            log::Level::Error => Color::Red,
+            log::Level::Debug => Color::Magenta,
+            _ => Color::Red,
+        };
+        let mut style = buf.style();
+        style.set_color(color);
+
+        writeln!(buf,
+            "{} [{}] {}",
+            Local::now().format("%Y-%m-%d %H:%M:%S"),
+            style.value(record.level()),
+            style.value(record.args()),
+        )
+    });
  
     builder.init();
 }
